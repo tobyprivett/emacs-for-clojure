566457e8a9529e12f9eaeaf71c8778ca6d0f8815
Toby Privett <toby@snaplab.co.uk>
2013-10-23 15:13:15 +0100

    Add melpa as a package source

--- user.el
+++ user.el
@@ -55,6 +55,11 @@
   '(add-to-list 'ac-modes 'nrepl-mode))
 (add-hook 'nrepl-mode-hook 'ac-nrepl-setup)
 
+(add-to-list 'load-path "~/.emacs.d/emacs-nav/")
+(require 'nav)
+(nav-disable-overeager-window-splitting)
+(global-set-key [f8] 'nav-toggle)
+
 ;; hippie expand - don't try to complete with file names
 (setq hippie-expand-try-functions-list (delete 'try-complete-file-name hippie-expand-try-functions-list))
 (setq hippie-expand-try-functions-list (delete 'try-complete-file-name-partially hippie-expand-try-functions-list))
